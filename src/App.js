9fd44632dc97fc54d9d71c29a5abd8ae59907171
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2022-10-11 15:14:07 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Happy Hacking! 🥰
+      </header>
+    </div>
+  );
+}
+
+export default App;
